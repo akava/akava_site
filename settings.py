94dd563e82e5fafe0d53b4ec3e2b18e62c27d3e3
Andrei Kavaleu <andrei.kavaleu@gmail.com>
2013-01-15 15:46:52 +0300

    Project structure reorganized

--- settings.py
+++ settings.py
@@ -102,10 +102,10 @@ MIDDLEWARE_CLASSES = (
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
-ROOT_URLCONF = 'blog.urls'
+ROOT_URLCONF = 'urls'
 
 # Python dotted path to the WSGI application used by Django's runserver.
-WSGI_APPLICATION = 'blog.wsgi.application'
+WSGI_APPLICATION = 'wsgi.application'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
@@ -122,7 +122,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'django.contrib.admin',
     'south',
-    'blog_app'
+    'blog'
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
