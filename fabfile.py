0b8d5aa9d5c3ab1eda8e39c1fd15fb6adcfa1f2b
Andrei Kavaleu <andrei.kavaleu@gmail.com>
2013-01-20 15:25:51 +0300

    Fabric

---
+++ fabfile.py
@@ -0,0 +1,23 @@
+from fabric.api import *
+import os
+
+env.hosts = [os.environ['HOSTING']]
+
+PROJECT_NAME = 'kavaleu_ru'
+PROJECT_DIR = '~/' + PROJECT_NAME
+
+
+def run_in_virtualenv(command):
+    run('source ~/.virtualenvs/%s/bin/activate && %s' %
+        (PROJECT_NAME, command))
+
+
+def deploy():
+    with cd(PROJECT_DIR):
+        run('git pull')
+        run_in_virtualenv('ls')
+        run_in_virtualenv('pip install -r requirements.txt')
+        run_in_virtualenv('python manage.py syncdb')
+        run_in_virtualenv('python manage.py migrate')
+        run_in_virtualenv('python manage.py clear_cache')
+        run('~/init/%s restart' % (PROJECT_NAME,))
