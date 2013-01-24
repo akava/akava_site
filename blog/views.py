3c06adc427b4b2c880d7eab926d4aa42e64336b5
Andrei Kavaleu <andrei.kavaleu@gmail.com>
2013-01-24 13:50:33 +0300

    List of entries on index page

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-
-
+from blog.models import Entry
 
 def index(request):
+    entries = Entry.objects.filter(status__exact=Entry.LIVE_STATUS)
+
     return render(request,
                   'blog/index.html',
-                  {})
+                  {'entries': entries})
